0dde017848cb91aea444a095035250c11a59e465
Burak Öztürk <burakozturk16@gmail.com>
2024-02-01 17:10:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import time
+import os
 
 def start():
+  test_env = os.environ.get("test_variable")
+  print("is environ loaded: " + test_env)
   print("service is started.")
+  
   while True:
     print("service is running...")
     time.sleep(10)
