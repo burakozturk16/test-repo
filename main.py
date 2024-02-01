4eb63038af0c7b2d837176096df483cb82438866
Burak Öztürk <burakozturk16@gmail.com>
2024-02-02 00:09:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def start():
   print("is environ loaded: " + test_env)
   print("service is started.")
   while True:
-    print("service is running...")
+    print("v0.0.4 service is running...")
     time.sleep(10)
 
 
